5fe9b9acba5d3a5a75935c267349537ab37c5d12
Tom Fielder <tom.fielder97@gmail.com>
2025-12-02 19:35:45 +0000

    Solve day 2 part 2 - regex is great 🎊

--- day2/index.ts
+++ day2/index.ts
@@ -13,7 +13,6 @@ export const part1 = (input: string) => {
     const rawInput = readInputToString(input)
 
     const rangeStrings = rawInput.split(",")
-    console.log(rangeStrings)
 
     // Parse range strings into actual range min and max values
 
@@ -23,8 +22,6 @@ export const part1 = (input: string) => {
 
         return { min, max }
     })
-
-    console.log(ranges)
 
     // For each range, add the invalid IDs to total
     let total: number = 0
@@ -52,4 +49,42 @@ const isNumberInvalid = (number: number) => {
         if (firstHalf === secondHalf) return true
     }
     return false
+}
+
+export const part2 = (input: string) => {
+    const rawInput = readInputToString(input)
+
+    const rangeStrings = rawInput.split(",")
+
+    // Parse range strings into actual range min and max values
+
+    const ranges = rangeStrings.map((range) => {
+        const min = parseInt(range.substring(0, range.indexOf("-")))
+        const max = parseInt(range.substring(range.indexOf("-") + 1))
+
+        return { min, max }
+    })
+
+    // For each range, add the invalid IDs to total
+    let total: number = 0
+
+    ranges.forEach((range) => {
+        // List through each ID in the range
+
+        for (let i = range.min; i <= range.max; i++) {
+            if (isNumberMadeOfRepeatingDigits(i)) total += i
+        }
+    })
+
+    return total
+}
+
+const isNumberMadeOfRepeatingDigits = (number: number) => {
+    const numberStr = number.toString()
+
+    // This regex checks if a string is made of repeating digits from the start to the end of the string
+    // Regex is great
+    const repeatingPattern = /^(\d+)\1+$/
+
+    return repeatingPattern.test(numberStr)
 }
--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
-import { part1, part2 } from "./day1"
+import { part1 as day1Part1, part2 as day1Part2 } from "./day1"
+import { part1 as day2Part1, part2 as day2Part2 } from "./day2"
 
 console.log("Advent of code solutions")
 
 console.log("====== Day 1 ======")
 
-console.log("Part 1:", part1("./day1/input.txt"))
-console.log("Part 2:", part2("./day1/input.txt"))
+console.log("Part 1:", day1Part1("./day1/input.txt"))
+console.log("Part 2:", day1Part2("./day1/input.txt"))
+
+console.log("====== Day 1 ======")
+
+console.log("Part 1:", day2Part1("./day2/input.txt"))
+console.log("Part 1:", day2Part2("./day2/input.txt"))
