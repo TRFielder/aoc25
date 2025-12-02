f34e62b6fc50c2951c5f35e3f8f5db8bac3fcca6
Tom Fielder <tom.fielder97@gmail.com>
2025-12-02 19:36:02 +0000

    Correct typo

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ console.log("====== Day 1 ======")
 console.log("Part 1:", day1Part1("./day1/input.txt"))
 console.log("Part 2:", day1Part2("./day1/input.txt"))
 
-console.log("====== Day 1 ======")
+console.log("====== Day 2 ======")
 
 console.log("Part 1:", day2Part1("./day2/input.txt"))
 console.log("Part 1:", day2Part2("./day2/input.txt"))
